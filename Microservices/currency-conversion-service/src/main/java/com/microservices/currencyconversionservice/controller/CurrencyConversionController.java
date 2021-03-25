a3ea28fa7b1edbe0b91cda8dca057e675816b856
Nicolas Saavedra <nicolassaavedra@Nicolass-MBP.fibertel.com.ar>
2021-03-25 03:14:08 -0300

    Added eureka and feign support. Api gateway basic project

--- Microservices/currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ Microservices/currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,37 +1,28 @@
 package com.microservices.currencyconversionservice.controller;
 
 import com.microservices.currencyconversionservice.model.CurrencyConversion;
-import org.springframework.http.ResponseEntity;
+import com.microservices.currencyconversionservice.rest.CurrencyExchangeProxy;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/currency-conversion-service")
 public class CurrencyConversionController {
+
+  private final CurrencyExchangeProxy currencyExchangeProxy;
 
   @GetMapping("/from/{from}/to/{to}/quantity/{quantity}")
   public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
                                                         @PathVariable String to,
                                                         @PathVariable BigDecimal quantity) {
-
-    HashMap<String, String> params = new HashMap<>();
-    params.put("from", from);
-    params.put("to", to);
-
-    ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate()
-      .getForEntity(
-        "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
-        CurrencyConversion.class,
-        params
-      );
-
-    CurrencyConversion responseEntityBody = responseEntity.getBody();
+    
+    CurrencyConversion responseEntityBody = currencyExchangeProxy.retrieveExchangeValue(from, to);
 
     return CurrencyConversion.builder()
       .id(responseEntityBody.getId())
