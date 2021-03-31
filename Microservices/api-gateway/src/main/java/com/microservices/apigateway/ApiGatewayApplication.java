0bb0d6afeaade8811b5cffbcfccf529632c266e8
Nicolas Saavedra <nicolassaavedra@Nicolass-MBP.fibertel.com.ar>
2021-03-31 01:41:20 -0300

    Added API gateway example

--- Microservices/api-gateway/src/main/java/com/microservices/apigateway/ApiGatewayApplication.java
+++ Microservices/api-gateway/src/main/java/com/microservices/apigateway/ApiGatewayApplication.java
@@ -3,8 +3,10 @@ package com.microservices.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This kind of ApiGateway implementation is useful for things that are common for all your microservices,
+// such as authentication, security, metrics, etc. It's also useful for endpoint routing.
 @SpringBootApplication
-public class ApiGatewayApplication {
+public class  ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
