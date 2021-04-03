df4f2450c63872cb41255c7553b08b23e006e769
Nicolas Saavedra <nicolassaavedra@Nicolass-MBP.fibertel.com.ar>
2021-04-03 04:23:27 -0300

    Added circuit breaker logic with default config

--- Microservices/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ Microservices/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +11,26 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class CircuitBreakerController {
 
-  @GetMapping("/sample-api")
+  @GetMapping("/sample-api-retry")
   @Retry(name = "sample-api", fallbackMethod = "hardCodedResponse")
   //Retries the call n times before throwing an exception
-  public String testController() {
+  public String retryController() {
     log.info("Trying connection with wrong url..");
     return new RestTemplate()
       .getForEntity("wrongUrl", String.class)
       .getBody();
   }
+
+  @GetMapping("/sample-api-circuit-breaker")
+  @CircuitBreaker(name = "default", fallbackMethod = "hardCodedResponse")
+  //Stops method execution after a certain amount of failures
+  public String circuitBreakerController() {
+    log.info("Trying connection with wrong url..");
+    return new RestTemplate()
+      .getForEntity("wrongUrl", String.class)
+      .getBody();
+  }
+
 
   public String hardCodedResponse(Exception e) { //You can specify other type of exceptions here
     return "Fallback response, error: " + e.getMessage();
