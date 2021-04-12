c657738bf1a9c1131ae2049170f06366744b20d3
Nicolas Saavedra <nicolassaavedra@Nicolass-MBP.fibertel.com.ar>
2021-04-12 01:45:14 -0300

    Added zipkin support

--- Microservices/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ Microservices/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -3,6 +3,7 @@ package com.microservices.currencyexchangeservice.controller;
 import com.microservices.currencyexchangeservice.Repository.CurrencyExchangeRepository;
 import com.microservices.currencyexchangeservice.model.CurrencyExchange;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,17 +15,19 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/currency-exchange")
+@Slf4j
 public class CurrencyExchangeController {
 
   private final Environment environment;
 
   private final CurrencyExchangeRepository currencyExchangeRepository;
-
+  
   @GetMapping("/from/{from}/to/{to}")
   public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
                                                 @PathVariable String to) {
     return Optional.ofNullable(currencyExchangeRepository.findByFromAndTo(from, to))
       .map(currencyExchange -> {
+        log.info("retrieveExchangeValue called with values from {} and to {}.", from, to);
         currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
         return currencyExchange;
       }).orElse(null);
